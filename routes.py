f26ad333fa3e0a6264f4603a82a454e32dd927f4
Sehaj Chawla <sehajchawla44@gmail.com>
2017-01-01 00:01:18 +0000

    Final Reminders App

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
 import forms
 from models import Task
 from datetime import datetime
@@ -17,6 +17,41 @@ def add():
         t = Task(title=form.title.data, date=datetime.utcnow())
         db.session.add(t)
         db.session.commit()
+        flash('Task added to the database')
         print('Submitted title', form.title.data)
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
+
+@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
+def edit(task_id):
+    task = Task.query.get(task_id)
+    form = forms.AddTaskForm()
+    if task:
+        if form.validate_on_submit():
+            task.title = form.title.data
+            task.date = datetime.utcnow()
+            db.session.commit()
+            flash('Task has been updated')
+            return redirect(url_for('index'))
+        form.title.data = task.title
+        return render_template('edit.html', form=form, task_id=task_id)
+    else:
+        flash('Task not found!')
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>', methods=['GET', 'POST'])
+def delete(task_id):
+    task = Task.query.get(task_id)
+    form2 = forms.DeleteForm()
+    if task:
+        if form2.validate_on_submit():
+            db.session.delete(task)
+            db.session.commit()
+            flash('Task has been updated')
+            return redirect(url_for('index'))
+
+        return render_template('delete.html', form=form2, task_id=task_id, title=task.title)
+    else:
+        flash('Task not found!')
+
+    return redirect(url_for('index'))
