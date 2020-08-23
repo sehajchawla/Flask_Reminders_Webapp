a22286ef82afb71bc7268046ed879feec32dd61a
Sehaj Chawla <sehajchawla44@gmail.com>
2020-08-23 06:15:32 +0100

    SQLAlchemy used to add db functionality

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
 
 from routes import *
 
--- routes.py
+++ routes.py
@@ -1,15 +1,22 @@
-from app import app
-from flask import render_template
-from forms import AddTaskForm
+from app import app, db
+from flask import render_template, redirect, url_for
+import forms
+from models import Task
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', current_title="Custome Title")
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
 
-@app.route('/about', methods=['GET', 'POST'])
-def about():
-    form = AddTaskForm()
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = forms.AddTaskForm()
     if form.validate_on_submit():
+        t = Task(title=form.title.data, date=datetime.utcnow())
+        db.session.add(t)
+        db.session.commit()
         print('Submitted title', form.title.data)
-    return render_template('about.html', form=form)
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
